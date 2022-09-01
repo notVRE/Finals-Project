5c0b5029161767daaff5f2d683717ae79bf3fb3c
notVRE <112544057+notVRE@users.noreply.github.com>
2022-09-01 22:55:16 +0800

    Add files via upload

---
+++ lp.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Landing Page
+    </title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="split left">
+        <a href="User/tlist.php"><h1>Table for User</h1></a>
+        <a href="User/add.php" class="btn">Add New User</a>
+      </div>
+      <div class="split right">
+        <a href="Agency/List.php"><h1>Table for Agency</h1></a>
+        <a href="Agency/Exam.php" class="btn">Add New Employee</a>
+      </div>
+    </div>
+
+    <script src="script.js"></script>
+  </body>
+</html>
